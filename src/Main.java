813e90537c9fdb341bf9056d567db49eccd7baa9
Lenovo_IT2 <citizen555@o2.pl>
2023-01-23 15:47:54 +0100

    everytime closer :)

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
-        System.out.println("AAAA GIT DZIAŁA xDDDDD");
-        System.out.println("I to jeszcze jak :D");
-        System.out.println("o tak :3");
+        System.out.println("Someday it will works :)");
     }
 }
---
+++ src/loginPanel.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class loginPanel {
+    private JPanel panel1;
+    private JTextField tfLogin;
+    private JPasswordField pfPassword;
+    private JButton btnLogin;
+    private JButton btnClose;
+}
