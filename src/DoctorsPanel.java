25c7c25e8e74b1e074fec89451b36610b0d46938
citizen5 <citizen555@o2.pl>
2023-02-04 23:19:33 +0100

    04.02

--- src/DoctorsPanel.java
+++ src/DoctorsPanel.java
@@ -9,6 +9,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DoctorsPanel extends JFrame{
+    PreparedStatement statement=null;
+    ResultSet result=null;
     private JButton btnBack;
     private JPanel doctorsPanel;
     private JTable tableDoctors;
@@ -32,7 +34,7 @@ public class DoctorsPanel extends JFrame{
     private JTextField tfEmail;
     private JLabel lEmail;
     private JTextField tfSalary;
-    private JButton btnSave;
+    private JButton btnAdd;
     private JButton btnDelete;
     private JTextField tfName;
     private JButton btnClear;
@@ -43,22 +45,13 @@ public class DoctorsPanel extends JFrame{
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setContentPane(doctorsPanel);
         this.setResizable(false);
-        this.setSize(1400,1000);
+        this.setSize(1400,700);
         this.setLocationRelativeTo(null);
         tableDoctors.setDefaultEditor(Object.class, null);
         showData();
         tableDoctors.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
         tableDoctors.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
 
-        // sets the look and feel to be that of the operating system's
-        try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (ClassNotFoundException |
-                 InstantiationException |
-                 IllegalAccessException |
-                 UnsupportedLookAndFeelException e) {
-            e.printStackTrace();
-        }
 
         btnBack.addActionListener(new ActionListener() {
             @Override
@@ -89,28 +82,68 @@ public class DoctorsPanel extends JFrame{
             @Override
             public void mouseClicked(MouseEvent e) {
                 super.mouseClicked(e);
-                DefaultTableModel model=(DefaultTableModel) tableDoctors.getModel();
+                DefaultTableModel tempModel =(DefaultTableModel) tableDoctors.getModel();
                 int selectedRow=tableDoctors.getSelectedRow();
-                tfName.setText(model.getValueAt(selectedRow,1).toString());
-                tfSurname.setText(model.getValueAt(selectedRow,2).toString());
-                tfBirthDate.setText(model.getValueAt(selectedRow,3).toString());
-                tfBirthPlace.setText(model.getValueAt(selectedRow,4).toString());
-                tfPesel.setText(model.getValueAt(selectedRow,5).toString());
-                tfAdres.setText(model.getValueAt(selectedRow,6).toString());
-                tfPostCode.setText(model.getValueAt(selectedRow,7).toString());
-                tfCity.setText(model.getValueAt(selectedRow,8).toString());
-                tfTelephone.setText(model.getValueAt(selectedRow,9).toString());
-                tfEmail.setText(model.getValueAt(selectedRow,10).toString());
-                tfSalary.setText(model.getValueAt(selectedRow,11).toString());
+                tfName.setText(tempModel.getValueAt(selectedRow,1).toString());
+                tfSurname.setText(tempModel.getValueAt(selectedRow,2).toString());
+                tfBirthDate.setText(tempModel.getValueAt(selectedRow,3).toString());
+                tfBirthPlace.setText(tempModel.getValueAt(selectedRow,4).toString());
+                tfPesel.setText(tempModel.getValueAt(selectedRow,5).toString());
+                tfAdres.setText(tempModel.getValueAt(selectedRow,6).toString());
+                tfPostCode.setText(tempModel.getValueAt(selectedRow,7).toString());
+                tfCity.setText(tempModel.getValueAt(selectedRow,8).toString());
+                tfTelephone.setText(tempModel.getValueAt(selectedRow,9).toString());
+                tfEmail.setText(tempModel.getValueAt(selectedRow,10).toString());
+                tfSalary.setText(tempModel.getValueAt(selectedRow,11).toString());
+            }
+        });
+        btnDelete.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                DefaultTableModel tempModel=(DefaultTableModel) tableDoctors.getModel();
+                int selectedRow=tableDoctors.getSelectedRow();
+
+                if(selectedRow==-1){
+                    JOptionPane.showMessageDialog(doctorsPanel,"Aby usunąć dane, wybierz wiersz z tabeli");
+                }else{
+                    try{
+                        String sql="DELETE FROM `lekarze` WHERE `IDlekarza` = "+tempModel.getValueAt(selectedRow,0).toString()+";";
+                        statement = DBConnection.dbConnection.prepareStatement(sql);
+                        statement.executeUpdate(sql);
+                        showData();
+                        JOptionPane.showMessageDialog(doctorsPanel,"Dane usunięto poprawnie");
+                        statement.close();
+                    }catch (SQLException ex){
+                        ex.printStackTrace();
+                    }
+
+                }
+                showData();
+
+            }
+        });
+        btnAdd.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                try{
+                    String sql="INSERT INTO  `lekarze` (`Imie`, `Nazwisko`, `DataUrodzenia`, `MiejsceUrodzenia`,`Pesel`,`AdresZamieszkania`,`KodPocztowy`,`Miasto`,`Telefon`,`Email`,`Wynagrodzenie`) VALUES ('"+tfName.getText()+"','"+tfSurname.getText()+"','"+tfBirthDate.getText()+"','"+tfBirthPlace.getText()+"','"+tfPesel.getText()+"','"+tfAdres.getText()+"','"+tfPostCode.getText()+"','"+tfCity.getText()+"','"+tfTelephone.getText()+"','"+tfEmail.getText()+"','"+Integer.parseInt(tfSalary.getText())+"');";
+                    statement = DBConnection.dbConnection.prepareStatement(sql);
+                    statement.executeUpdate(sql);
+                    showData();
+                    JOptionPane.showMessageDialog(doctorsPanel,"Dane dodano poprawnie");
+                    statement.close();
+                }catch (SQLException ex){
+                    ex.printStackTrace();
+                }
+
+
             }
         });
     }
 
 
     public void showData(){
-        PreparedStatement statement=null;
-        ResultSet result=null;
-
         try{
 
             String sql="SELECT * From lekarze";
--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,15 @@ public class Main {
 
         JFrame loginPage = new LoginPanel();
         loginPage.setVisible(true);
+        //setting current Windows version look and feel
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException |
+                 InstantiationException |
+                 IllegalAccessException |
+                 UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
 
     }
 }
